ed9898cf42dfa79ebd32557b122af65f3bd14d4a
Dmitry Andreyev <Krygliy.ya@gmail.com>
2025-04-14 00:11:44 +0700

    refactor

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,9 +12,13 @@ export const Card: React.FC<Props> = ({ date, title, description, author }) => {
   return (
     <div className="article__card">
       <div className="article__card__date">{date} </div>
-      <h3 className="article__card__title">{title}</h3>
-      <p className="article__card__description">{description}</p>
-      <div className="article__card__author">{author}</div>
+      <a href="#ссылка на статью">
+        <h3 className="article__card__title">{title}</h3>
+        <p className="article__card__description">{description}</p>
+      </a>
+      <a href="#ссылка на автора">
+        <div className="article__card__author">{author}</div>
+      </a>
     </div>
   );
 };
